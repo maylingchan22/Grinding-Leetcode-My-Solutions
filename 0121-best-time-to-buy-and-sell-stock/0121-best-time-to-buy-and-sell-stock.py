79f10ef0f9429db06b5777ce4fd414157dc08e2c
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-10-12 03:17:34 -0400

    Time: 2551 ms (27.26%), Space: 25.1 MB (38.11%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -7,4 +7,5 @@ class Solution:
                 min_price = prices[i]
             elif prices[i] - min_price > max_profit:
                 max_profit = prices[i] - min_price
+                
         return max_profit
