027ebab6e9183e05461d820dd092067803b49981
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-10-16 02:06:47 -0400

    Time: 60 ms (47.33%), Space: 13.9 MB (9.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,4 +1,4 @@
 class Solution:
     def fib(self, n: int) -> int:
         golden_ratio = (1 + (5**0.5)) / 2
-        return int(round((golden_ratio**n) / (5**0.5)))
+        return int(round((golden_ratio ** n) / (5**0.5)))
