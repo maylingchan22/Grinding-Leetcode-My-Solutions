774c98977c992693c86a1fd4e175f3489b92bd00
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-10-21 21:50:57 -0400

    Time: 444 ms (71.86%), Space: 16.4 MB (81.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,7 +3,7 @@ class Solution:
         def dfs(grid, i, j):
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
                 return
-            grid[i][j] = '#'
+            grid[i][j] = "#"
             dfs(grid, i+1, j)
             dfs(grid, i-1, j)
             dfs(grid, i, j+1)
@@ -17,3 +17,4 @@ class Solution:
                     dfs(grid, i, j)
                     count += 1
         return count
+        
