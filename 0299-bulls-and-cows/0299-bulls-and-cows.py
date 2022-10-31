a0813e8a5b55bd073cd40b292e3296f342770441
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-10-31 03:37:38 -0400

    Time: 66 ms (74.67%), Space: 13.9 MB (80.58%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.py
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -3,11 +3,12 @@ class Solution:
         h = Counter(secret)
         bulls = cows = 0
         for idx, ch in enumerate(guess):
-            if ch == secret[idx]:
-                bulls += 1
-                cows -= int(h[ch] <= 0)
-            else:
-                cows += int(h[ch] > 0)
-            h[ch] -= 1
+            if ch in h:
+                if ch == secret[idx]:
+                    bulls += 1
+                    cows -= int(h[ch] <= 0)
+                else:
+                    cows += int(h[ch] > 0)
+                h[ch] -= 1
         return "{}A{}B".format(bulls, cows)
-     
+    
