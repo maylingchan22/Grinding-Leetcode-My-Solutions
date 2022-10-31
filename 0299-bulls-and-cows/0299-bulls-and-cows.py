341bf877d5eb2562a11894438abd34f211f8d37c
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-10-31 03:29:40 -0400

    Time: 100 ms (23.90%), Space: 13.9 MB (80.58%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        h = Counter(secret)
+        bulls = cows = 0
+        for idx, ch in enumerate(guess):
+            if ch == secret[idx]:
+                bulls += 1
+                cows -= int(h[ch] <= 0)
+            else:
+                cows += int(h[ch] > 0)
+            h[ch] -= 1
+        return "{}A{}B".format(bulls, cows)
